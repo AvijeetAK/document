2bf4e8013f6862a36c3fe93c16215ffd2513a321
AvijeetAk <avijeetkumar1994@gmail.com>
2023-03-03 15:07:16 +0530

    document

--- dom.js
+++ dom.js
@@ -4,3 +4,13 @@ console.log(headerTitle);
 headerTitle.innerHTML = '<h3>Hello</h3>';
 
 headerTitle.textContent = 'Hello';
+
+var items = document.getElementsByClassName('list-group-item');
+console.log(items);
+
+items[2].style.backgroundColor = 'green';
+
+for(var i=0; i<items.length; i++)
+{
+    items[i].style.fontWeight = 'bold';
+}
